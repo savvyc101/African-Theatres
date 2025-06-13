f31a0550aaaab83f8e6719aae380604e531fda9b
savvyc101 <techsavvycelebrants@gmail.com>
2025-06-14 08:18:28 +1000

    Added package-lock.json

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Navbar3 } from "./components/Navbar3";
+import { Header102 } from "./components/Header102";
+import { Layout194 } from "./components/Layout194";
+import { Layout245 } from "./components/Layout245";
+import { Layout4 } from "./components/Layout4";
+import { Layout194_1 } from "./components/Layout194_1";
+import { Testimonial4 } from "./components/Testimonial4";
+import { Cta45 } from "./components/Cta45";
+import { Cta2 } from "./components/Cta2";
+import { Footer7 } from "./components/Footer7";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar3 />
+      <Header102 />
+      <Layout194 />
+      <Layout245 />
+      <Layout4 />
+      <Layout194_1 />
+      <Testimonial4 />
+      <Cta45 />
+      <Cta2 />
+      <Footer7 />
+    </div>
+  );
+}
